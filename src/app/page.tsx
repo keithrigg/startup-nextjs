c03c6c37f46a8295868627ac47042b338f8ceed4
Keith Rigg <keith.rigg@inovize.com>
2025-05-18 19:26:55 +0100

    Remove brand logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
       <Hero />
       <PowerSection />
       <Features />
-      <Brands />
+      {/*<Brands />*/}
       <Services />
       <WhyUsSectionOne />
       <WhyUsSectionTwo />
