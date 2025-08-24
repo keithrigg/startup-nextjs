507fafe28e9e739778ab3fe0248e71f7e91ad3cd
Keith Rigg <keith.rigg@inovize.com>
2025-08-24 10:05:27 +0200

    Update images

--- src/components/WhyUs/WhyUsSectionOne.tsx
+++ src/components/WhyUs/WhyUsSectionOne.tsx
@@ -53,13 +53,13 @@ const WhyUsSectionOne = () => {
             <div className="w-full px-4 lg:w-1/2">
               <div className="relative mx-auto aspect-[25/24] max-w-[500px] lg:mr-0">
                 <Image
-                  src="/images/about/about-image.svg"
+                  src="/images/why/why_work_with_inovize_integration_architects.jpg"
                   alt="about-image"
                   fill
                   className="mx-auto max-w-full drop-shadow-three dark:hidden dark:drop-shadow-none lg:mr-0"
                 />
                 <Image
-                  src="/images/about/about-image-dark.svg"
+                  src="/images/why/why_work_with_inovize_integration_architects.jpg"
                   alt="about-image"
                   fill
                   className="mx-auto hidden max-w-full drop-shadow-three dark:block dark:drop-shadow-none lg:mr-0"
